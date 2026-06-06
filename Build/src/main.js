3238dea8e015f2f7997fbae2fd99ef36fa2d5534
NellowTCS <neeljaiswal23@gmail.com>
2026-06-06 13:15:10 -0600

    Add updato

--- Build/src/main.js
+++ Build/src/main.js
@@ -1,3 +1,24 @@
+import { Updato } from "@nellowtcs/updato";
+import { UpdateNotification } from "@nellowtcs/updato/update-ui";
+
+// Keep
+const updater = Updato.init(
+  {
+    repo: "NellowTCS/Web-Template",
+    mode: "version",
+    current: "0.1.0",
+  },
+  {
+    onUpdate: (info) => {
+      new UpdateNotification(updater, {
+        heading: `v${info.latest} ready`,
+      }).show(info);
+    },
+    onError: (err) => console.warn("Updato:", err.message),
+    onProgress: (pct, file) => console.log(`Updato: ${pct}% - ${file}`),
+  },
+);
+
 function testFunction() {
     console.log('Button clicked!');
     console.warn('This is a warning');
